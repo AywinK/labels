ed2b8ada914d33e6e05939c8b2d3c447d3c47ad9
Aywin <AywinK@hotmail.com>
2023-11-06 17:30:03 +0000

    adds logic for array of part objs, creates class for parts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("paste", e => {
     e.preventDefault();
     const dataArr = e.clipboardData.getData("text");
+    console.log(dataArr);
     const joinedArr = [];
     let str = "";
     for (data of dataArr) {
@@ -12,17 +13,36 @@ document.addEventListener("paste", e => {
             str += data
         }
     }
+    joinedArr.push(str);
+
     console.log(joinedArr);
     const joinedArrFilteredEmptyStr = joinedArr.filter(element => (element !== ""));
     console.log(joinedArrFilteredEmptyStr);
     const arrOfParts = [];
     for (let i = 0; i < joinedArrFilteredEmptyStr.length; i += 5) {
-        arrOfParts.push(joinedArrFilteredEmptyStr[i]);
+        let tempArr = [];
+        tempArr.push(joinedArrFilteredEmptyStr[i]);
+        tempArr.push(joinedArrFilteredEmptyStr[i + 1]);
+        tempArr.push(joinedArrFilteredEmptyStr[i + 2]);
+        tempArr.push(joinedArrFilteredEmptyStr[i + 3]);
+        tempArr.push(joinedArrFilteredEmptyStr[i + 4]);
+        arrOfParts.push(tempArr);
     }
-for (const arr of arrOfParts) {
-    for (let i = 1; i < joinedArrFilteredEmptyStr.length; i += 5) {
-        arr.push(joinedArrFilteredEmptyStr[i]);
+
+    console.log(arrOfParts);
+    const arrOfPartsObj = [];
+    for (const arr of arrOfParts) {
+        arrOfPartsObj.push(new Part(...arr));
+    }
+    console.table(arrOfPartsObj);
+});
+
+class Part {
+    constructor(...partdetails) {
+        this.partNumber = partdetails[0];
+        this.sheetNumber = partdetails[1];
+        this.van = partdetails[2];
+        this.edging = partdetails[3];
+        this.location = partdetails[4];
     }
 }
-    console.log(arrOfParts);
-})
